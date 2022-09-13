1dc4ca397f560c266284d46aae3e97e30229d813
leopnt <leopnt.dev@gmail.com>
2022-09-13 17:31:28 +0200

    set debug_ui to singleton

--- src/debug_ui.gd
+++ src/debug_ui.gd
@@ -1,12 +1,22 @@
 extends Control
 
-onready var _logs = get_node("HSplitContainer/Logs")
-
+var _logs: TextEdit = null
+var _debug_ui: Control = null
 
 func _ready():
     pass
+
+func link_scene(debug_ui: Control):
+    _debug_ui = debug_ui
+    _logs = debug_ui.get_node("HSplitContainer/Logs")
 
 
 func logln(msg: String):
     _logs.text += msg
     _logs.text += "\n"
+    _autoscroll_logs()
+
+
+func _autoscroll_logs():
+    var cl = _logs.get_line_count()
+    _logs.cursor_set_line(cl)
--- src/main.gd
+++ src/main.gd
@@ -1,8 +1,4 @@
 extends Node2D
 
-onready var debug_ui = get_node("CanvasLayer/DebugUI")
-
 func _ready():
-    print("wtf")
-    debug_ui.logln("test")
-    debug_ui.logln("ta grand mere")
+    DebugUI.link_scene($CanvasLayer/DebugUI)
